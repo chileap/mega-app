8a4b633db5cf78b302029d7bfbb800f9b77da5dd
Chileap Chhin <chileapchhin@gmail.com>
2024-03-08 14:07:21 +0700

    fix rspec

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -16,6 +16,19 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   end
 end
 
+Capybara.register_driver :selenium do |app|
+  options = ::Selenium::WebDriver::Chrome::Options.new
+
+  options.add_argument('--headless')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1366,720')
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :selenium
 Capybara.default_max_wait_time = 10
 
 # Add a route for easily switching workspaces in system tests
